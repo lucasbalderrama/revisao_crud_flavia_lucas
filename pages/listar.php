3e3fe6f5ebbd2e2f143b16c700bd82c285cba460
lucasbalderrama <abreulucas3715@gmail.com>
2025-07-29 12:14:45 -0300

    listar

--- pages/listar.php
+++ pages/listar.php
@@ -1,6 +1,22 @@
+<!-- Flávia Glenda e Lucas Randal -->
 <?php
+session_start();
 include '../config/conexao.php';
 
+$sql = "
+    SELECT 
+        pokemons.nome_pokemon,
+        pokemons.tipo_pokemon,
+        pokemons.localizacao_pokemon,
+        pokemons.registro_pokemon,
+        pokemons.obs_pokemon,
+        pokemons.hp_pokemon,
+        pokemons.ataque_pokemon,
+        pokemons.defesa_pokemon
+    FROM pokemons
+";
+
+$resultado = $conn->query($sql);
 ?>
 
 <!DOCTYPE html>
@@ -9,8 +25,9 @@ include '../config/conexao.php';
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Listar Pokemons</title>
+  <title>Listar Pokémons</title>
   <link rel="stylesheet" href="../src/css/styles.css">
+  <link rel="stylesheet" href="../src/css/tabelas.css">
   <link rel="icon" type="image/x-icon" href="../src/assets/img/pokebola.png">
 </head>
 
@@ -43,11 +60,45 @@ include '../config/conexao.php';
       <li><img class="dell" src="../src/assets/img/pokemon 9.png" alt=""></li>
       <li><img class="dell" src="../src/assets/img/pokemon 10.png" alt=""></li>
     </ul>
+    <main>
+      <h1 id='pok-h1'>LISTA DE POKÉMONS CADASTRADOS:</h1>
+      <div class="pokemons" style="overflow-x:auto;">
+        <?php if ($resultado->num_rows > 0): ?>
+          <table>
+            <thead>
+              <tr>
+                <th>Nome</th>
+                <th>Tipo</th>
+                <th>Localização</th>
+                <th>Registro</th>
+                <th>Observações</th>
+                <th>HP</th>
+                <th>Ataque</th>
+                <th>Defesa</th>
+              </tr>
+            </thead>
+            <tbody>
+              <?php while ($row = $resultado->fetch_assoc()): ?>
+                <tr>
+                  <td><?php echo ($row['nome_pokemon']); ?></td>
+                  <td><?php echo ($row['tipo_pokemon']); ?></td>
+                  <td><?php echo ($row['localizacao_pokemon']); ?></td>
+                  <td><?php echo ($row['registro_pokemon']); ?></td>
+                  <td><?php echo ($row['obs_pokemon']); ?></td>
+                  <td><?php echo ($row['hp_pokemon']); ?></td>
+                  <td><?php echo ($row['ataque_pokemon']); ?></td>
+                  <td><?php echo ($row['defesa_pokemon']); ?></td>
+                </tr>
+              <?php endwhile; ?>
+            </tbody>
+          </table>
+        <?php else: ?>
+          <p style="color: red; margin-top: 30px; text-align: center; font-family:Arial, Helvetica, sans-serif; font-size:18px;">Nenhum pokémon cadastrado.</p>
+        <?php endif; ?>
+      </div>
+    </main>
+
   </div>
-
-  <main>
-
-  </main>
 
   <footer class="footer">
     <div class="footer-container">
